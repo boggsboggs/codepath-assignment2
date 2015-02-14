5a13b1e6eb95246a6004ab70c75c9b7d0608461b
John Boggs <jboggs@twitter.com>
2015-02-13 20:21:16 -0800

    init

---
+++ Project/BridgingHeader.m
@@ -0,0 +1,10 @@
+//
+//  BridgingHeader.m
+//  Project
+//
+//  Created by John Boggs on 2/13/15.
+//  Copyright (c) 2015 Codepath. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIImageView+AFNetworking.h>
